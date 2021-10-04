f69d27e64b744ab1b3c59f3ac77453033f17d6af
Vladimir Bodurov <vladimir@bodurov.com>
2021-10-03 20:19:08 -0700

    add rigid body

--- quest-experiments-unity/Assets/HandsGrabControl/Scripts/GrabControllerInitializer.cs
+++ quest-experiments-unity/Assets/HandsGrabControl/Scripts/GrabControllerInitializer.cs
@@ -51,11 +51,14 @@ namespace OvrAltGrab
             var isThumb = bone.Id == OVRSkeleton.BoneId.Hand_Thumb3;
 
             // aura
-            var cc2 = aura.AddComponent<CapsuleCollider>();
-            cc2.direction = 0;
-            cc2.center = isPinky ? new Vector3(-0.006f, 0, 0) : new Vector3(-0.008f, 0, 0);
-            cc2.height = 0.048f;
-            cc2.radius = isThumb ? 0.016f : 0.0112f;
+            var cc = aura.AddComponent<CapsuleCollider>();
+            cc.direction = 0;
+            cc.center = isPinky ? new Vector3(-0.006f, 0, 0) : new Vector3(-0.008f, 0, 0);
+            cc.height = 0.048f;
+            cc.radius = isThumb ? 0.016f : 0.0112f;
+
+            var rb = aura.AddComponent<Rigidbody>();
+            rb.isKinematic = true;
 
             var fa = aura.AddComponent<FingerAura>();
             fa.SetTouchDetector(bone.Id, _detector);
